fcd6fe4ac2de6286ac8c6b70216a1d1b4f5b8dc6
Hardyn1 <amki.business@gmail.com>
2024-07-22 10:48:42 +0300

    TABS lesson done, + Added MODALWINDOW

---
+++ main.js
@@ -0,0 +1,94 @@
+// Inputs
+let rangeTl = document.getElementById("tlr");
+let rangeTr = document.getElementById("trr");
+let rangeBl = document.getElementById("blr");
+let rangeBr = document.getElementById("brr");
+// Inputs div
+let resultTl = document.getElementById("result-tlr");
+let resultTr = document.getElementById("result-trr");
+let resultBl = document.getElementById("result-blr");
+let resultBr = document.getElementById("result-brr");
+
+// Навесим обработчик событий сразу на все input,
+// чтобы соединить с div result и выводить значения.
+let inputs = document.querySelectorAll(".input");
+let cube = document.getElementById("cube");
+// Cube является главным в этой программе,
+// на нем и будем изменять.
+
+// Написать функцию которая будет изменять радиус.
+// Надо связать значения div и range.
+// Связали каждый range с результатом div.
+function changeRadius() {
+    resultTl.innerHTML = rangeTl.value;
+    resultTr.innerHTML = rangeTr.value;
+    resultBl.innerHTML = rangeBl.value;
+    resultBr.innerHTML = rangeBr.value;
+
+    cube.style.borderRadius = rangeTl.value + "px " + rangeTr.value + "px " + rangeBr.value + "px " + rangeBl.value + "px ";
+    // cube.style.borderRadius = `${rangeTl.value}px ${rangeTr.value}px ${rangeBr.value}px ${rangeBl.value}px`;
+
+    // updateInputsValueText();
+};
+
+// Теперь надо чтобы каждый из углов соединялся с углами куба.
+
+// Так как в querySelectorAll возвращается коллекция значений.
+// То используем цикл for of.
+for (let node of inputs) {
+    // console.log(node); // В переменной node хранятся наши inputs.
+    node.addEventListener("input", changeRadius); // Повесим событие input которое срабатывает на любое изменение.
+    //В качестве 2 аргумента передаем функцию связанных значений.
+    // Функция связывает результат и значение value.
+};
+
+/* 1.Взять CSS генератор который мы писали на уроке и добавить кнопку,
+   при нажатии на которую мы будем выводить весь результат наших inputs в какой то div
+   в виде стиля css соответствующий стилю border radius.
+ */
+
+let inputsResult = document.getElementById("inputs-result");
+let btn = document.getElementById("button");
+
+function totalResult() {
+    let inputArr = Array.from(inputs)
+    .map(function(input){
+        return input.value;
+    });
+
+    let inputValue = inputArr.join(' ');
+
+    inputsResult.textContent = inputValue;
+};
+
+
+btn.addEventListener("click", totalResult);
+
+
+
+
+
+
+
+
+
+
+
+// function updateInputsValueText() {
+//     const values = Array.from(inputs)
+//         .map(function(input) {
+//             return input.value;
+//         })
+//         .join(' ');
+
+//     inputsResult.textContent = values;
+// }
+
+// btn.addEventListener("click", updateInputsValueText);
+
+
+
+
+
+
+
---
+++ tabs/main.js
@@ -0,0 +1,27 @@
+let tabs = document.getElementById("tabs"),
+    content = document.querySelectorAll(".content");
+// ЕСЛИ HTML КОЛЛЕЦКИЯ ТО ЛУЧШЕ ПРОБЕГАТЬ ЦИКЛОМ FOR OF.
+function changeClass(el) {
+    // console.log(tabs.children);Выводит коллекцию и можно применить цикл for.
+    for(let i = 0; i < tabs.children.length; i ++) {
+        tabs.children[i].classList.remove("active"); //classList псевдомассив через метод remove можно удалить класс.
+        // Пр помощи цикла мы удалили у всех класс active.
+    };
+    console.log(el); // <div class="tab-btn active" data-btn="2">2</div>
+    el.classList.add("active");
+     // Сначала у всех класс active сняли, затем добавили.
+};
+tabs.addEventListener("click", function(event){
+    let currTab = event.target.dataset.btn; //event.target по какому элементу текущему произошел клик.
+    // ДАТА АТРИБУТЫ СЛУЖАТ ЧТОБЫ СВЯЗАТЬ HTML / JAVASCRIPT.
+    // console.log(event.target); Выводит текущее значение. <div class="tab-btn" data-btn="2">2</div>
+    // console.log(currTab); 
+    changeClass(event.target);
+    // Теперь надо связать между собой data атрибуты.
+    for(let a = 0; a < content.length; a++) { // CONTENT.LENGTH - ЭТО КОНЕЦ НАШЕЙ КОЛЛЕКЦИИ.
+        content[a].classList.remove("active");
+        if(content[a].dataset.content === currTab) { //ЛУЧШЕ ЧИТАТЬ КОД: ЕСЛИ В КОЛЛЕКЦИИ CONTENT[A] ЕСЛИ В DATASET.CONTENT РОВНЯЕТСЯ ТЕКУЩЕМУ TABS.
+            content[a].classList.add("active");
+        };
+    };
+});
