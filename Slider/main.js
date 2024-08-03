4d34c239f8110f5b8dfa68fc956eab53236547d5
Hardyn1 <amki.business@gmail.com>
2024-08-03 22:01:58 +0300

    lesson 20 is done

--- Slider/main.js
+++ Slider/main.js
@@ -1,39 +1,104 @@
-let prev = document.getElementById("btn-prev"),
-    next = document.getElementById("btn-next"),
-    slides = document.querySelectorAll(".slide"),
-    dots = document.querySelectorAll(".dot");
+// 2. Взять SLIDER с урока, и добавить новый функционал, а именно:
+// - при нажатии на точки снизу слайдера мы должны попадать на нужный нам слайд.
+// Нажимаем на первую точку - мы на первом слайде
+// Нажимаем на вторую точку - мы на втором слайде, и тд
+// - активная точка должна закрашиваться цветом ( цвет на ваш выбор )
+// - доработать слайдер так, чтобы после последнего слайда шел первый, и у нас начинался слайдер заново
+// - когда мы дошли до первого слайда , сделать так , чтобы слайдер не останавливался а переходил на последний слайд
+// вообщем слайдер должен работать циклично
 
-let index = 0;
+const slides = document.querySelectorAll(".slides img"); // все img элементы в slides
+let slideIndex = 0; // текущее значение
+let intervalId = null; // Нету значение
+/* 
+    initializeSlider()
+    Событие DOMContentLoaded гарантирует, что DOM готов. Можно искать узлы по нему и не бояться, что что-то не догрузилось.
+    Событие load дополнительно гарантирует, что все стили и картинки готовы.
+    В этот момент размеры элементов на страницы посчитаны верно и доступны.
+*/
 
-// Функция переключения слайда.
-function activeSlide(n){
-    for(let slide of slides) {
-        // При клике убираем класс актив
-        slide.classList.remove("active");
+
+function initializeSlider(){
+    
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    };
+    
+};
+
+document.addEventListener("DOMcontentLoaded", initializeSlider);
+
+function showSlide(index){
+
+    if(index >= slides.length){
+        slideIndex = 0;
     }
-    slides[n].classList.add("active");
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }  else {
+        slideIndex = index;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+
 };
 
+function prevSlide(){
+    clearInterval(intervalId); // stops interval
+    slideIndex--;
+    showSlide(slideIndex);
+    intervalId = setInterval(nextSlide, 5000);
+};
 
-// Переключает след, слайд.
-function nextSlide() {
-    // Строка проверят является ли мы на последнем слайде или нет.
-    if(index == slides.length - 1) {
-        index = slides.length - 1; // Остаемся на этом же слайде
-    } else {
-        index ++;
-        activeSlide(index);
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+    intervalId = setInterval(nextSlide, 5000);
+};
+
+const dots = document.querySelectorAll(".dot");
+let dotIndex = 0;
+
+function firstDotSlide(){
+
+    if(dots[dotIndex] >= slides[slideIndex = 0]){
+        dots[dotIndex] = showSlide(); 
     };
+
 };
 
-function prevSlide() {
-    if(index == 0) {
-        index = 0;
-    } else {
-        index --;
-        activeSlide(index);
+
+function secondDotSlide(){
+
+    if(dots[dotIndex] >= slides[slideIndex = 1]){
+        dots[dotIndex] = showSlide();
     };
+     
 };
 
-next.addEventListener("click", nextSlide);
-prev.addEventListener("click", prevSlide);
+function thirdDotSlide(){
+
+    if(dots[dotIndex] >= slides[slideIndex = 2]) {
+        dots[dotIndex] = showSlide();
+    };
+
+};
+
+function changeDotColor(){
+
+    dots.forEach(dott =>{
+     dott.addEventListener("click",() => {
+        dots.forEach(d =>d.classList.remove("clicked"));
+        dott.classList.add("clicked");
+        dott.classList.remove("gray");
+    });
+    
+});
+
+};
+
+changeDotColor();
--- tabs/main.js
+++ tabs/main.js
@@ -1,11 +1,11 @@
 let tabs = document.getElementById("tabs"),
     content = document.querySelectorAll(".content");
-// ЕСЛИ HTML КОЛЛЕЦКИЯ ТО ЛУЧШЕ ПРОБЕГАТЬ ЦИКЛОМ FOR OF.
+// ЕСЛИ HTML КОЛЛЕКЦИЯ ТО ЛУЧШЕ ПРОБЕГАТЬ ЦИКЛОМ FOR OF.
 function changeClass(el) {
     // console.log(tabs.children);Выводит коллекцию и можно применить цикл for.
     for(let i = 0; i < tabs.children.length; i ++) {
         tabs.children[i].classList.remove("active"); //classList псевдомассив через метод remove можно удалить класс.
-        // Пр помощи цикла мы удалили у всех класс active.
+        // При помощи цикла мы удалили у всех класс active.
     };
     console.log(el); // <div class="tab-btn active" data-btn="2">2</div>
     el.classList.add("active");
